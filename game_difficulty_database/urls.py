6f246bf0f8090add38dfab3f290eed01d181254e
Joe Brookes <joe.brookes@students.codeinstitute.net>
2024-09-23 11:27:12 +0000

    Implement add game functionality1

--- game_difficulty_database/urls.py
+++ game_difficulty_database/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.game_list, name='game_list'),
     path('game/<int:game_id>/', views.game_detail, name='game_detail'),
+    path('add/', views.add_game, name='add_game'),  # Add this line if it's missing
 ]
     # Add other paths as needed
