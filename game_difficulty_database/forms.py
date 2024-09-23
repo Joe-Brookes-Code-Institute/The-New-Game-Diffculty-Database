1130e32700c7420caa82702bc49a5a2c4342b93d
Joe Brookes <joe.brookes@students.codeinstitute.net>
2024-09-23 11:38:09 +0000

    Update GameForm to match current Game model

--- game_difficulty_database/forms.py
+++ game_difficulty_database/forms.py
@@ -4,4 +4,6 @@ from .models import Game
 class GameForm(forms.ModelForm):
     class Meta:
         model = Game
-        fields = ['name', 'difficulty', 'release_date', 'NGplus', 'description']
+        fields = ['name', 'difficulty', 'release_date', 'description', 
+                  'enemy_health', 'player_health', 'resources', 
+                  'unlockable', 'new_game_plus']
