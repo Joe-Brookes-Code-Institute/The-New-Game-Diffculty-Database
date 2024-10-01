c3ff42d9b477bb7eafa4cdf66e732da4d3005912
Joe Brookes <joe.brookes@students.codeinstitute.net>
2024-10-01 13:36:53 +0000

    Heroku fixed?

--- Game_Datebase/settings.py
+++ Game_Datebase/settings.py
@@ -31,10 +31,14 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')  # Fallback for local dev
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS += ['.gitpod.io', '.ws.codeinstitute-ide.net']
+DEBUG = True
+
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS += ['.gitpod.io', '.ws.codeinstitute-ide.net']
+
+ALLOWED_HOSTS = ['8000-joebrookesc-thenewgamed-6tlgqcr0ma2.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://*.codeinstitute-ide.net,https://*.gitpod.io').split(',')
 
@@ -99,8 +103,12 @@ WSGI_APPLICATION = 'Game_Datebase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///" + str(BASE_DIR / "db.sqlite3")))
+# }
+
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///" + str(BASE_DIR / "db.sqlite3")))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
